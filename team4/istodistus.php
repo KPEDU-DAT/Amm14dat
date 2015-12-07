75dd744f9011d47b7434c9b6253dd04396f7e8c4
Johan Handelin <johanhandelin@cosmo.kpedu.fi>
2015-12-07 12:52:26 +0200

    versio

---
+++ team4/istodistus.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta charset="utf-8">
+      <title>Luo isännöitsijäntodistus</title>
+  </head>
+  <body>
+    <form>
+      <input type="date" name="pvm" placeholder="">
+      <input type="text" name="kos" placeholder="Kiinteistön osoite">
+      <input type="text" name="ttyyppi" placeholder="Talotyyppi">
+      <input type="text" name="ktark" placeholder="Käyttötarkoitus">
+      <input type="text" name="pAla" placeholder="Kiinteistön pinta-ala">
+      <input type="text" name="hlkm" placeholder="Huoneiden lukumäärä">
+    </form>
+  </body>
+</html>
