5c3b9bff7127985f65f94ba1fc45c51ad078e4a3
Jonas Handelin <jonashandelin@cosmo.kpedu.fi>
2015-12-07 13:20:18 +0200

    Asuntojen poiston php-koodin pohja

---
+++ Team3/apoi.php
@@ -0,0 +1,19 @@
+<?php
+$my= new mysqli ('localhost','data15','aJrHfybLxsLU76rV','data15');
+    if($my->mysql_errno){
+        die("MySQL, virhe yhteyden luonnissa". $my->connect_error);
+    }
+?>
+<?php
+#Otetaan lomakkeesta se jonka mukaan halutaan poistaa asunto
+$ = $_POST[''];
+if ($) {
+		$sql="DELETE FROM tietokanta WHERE joku='$';";
+		if($tulos = $my->query($sql)){
+			echo "<h2>Asunto poistettu</h2>";
+			}
+		else {
+			echo "<h2> Asunnon poisto ei onnistunut</h2>";
+		}
+		}
+	?>
