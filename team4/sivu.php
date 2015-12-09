312a370c651c55f5a97ced65afdfd5f74f433145
Toni Isosaari <toniisosaari@cosmo.kpedu.fi>
2015-12-09 10:37:26 +0200

    Pääsivu

--- team4/sivu.php
+++ team4/sivu.php
@@ -24,11 +24,17 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-  </head>
+<style>
+table, th, td {
+    border: 1px solid black;
+    }
+    </style>  
+
+</head>
 
   <body>
 
-    <nav class="navbar navbar-inverse navbar-fixed-top">
+    <nav class="navbar navbar-inverse navbar-default">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -39,9 +45,9 @@
           </button>
           <a class="navbar-brand" href="#"></a>
         </div>
-        <div class="col-md-12 col-xs-12 col-lg-12">
+        
         <h3>JS Asunto Ou</h3>
-        </div>
+        
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
             <li class="active"><a href="#">Etusivu</a></li>
@@ -51,15 +57,48 @@
       </div>
     </nav>
 
-    <div class="container">
+    <div class="container" style="padding-top:2cm;">
+<div class="col-md-12 col-xs-12 col-lg-12 table-responsive" style="padding-top:2cm;">    
+<form action="" method="POST">
+</label>Haku parametri</label>
+<input type="text" name="hakua"></input>
+<label>Haku indeksi</label>
+<input type="text" name="hakub"></input>
+<button type="submit" name="nappi">Haku</button>
+</form>
+<table>
+<tbody>
+<tr><th>Osoite</th><th>Pnro</th><th>Kaupunki</th><th>Pinta-ala</th><th>Hinta</th><th>Tyyppi</th><th>Kerros</th><th>Asunto numero</th><th>Esittely</th><th>Rakennusvuosi</th></tr>
+<?php
+$nappi = $_POST['nappi'];
+$hakua = $_POST['hakua'];
+$hakub = $_POST['hakub'];
+$my=mysqli_connect("localhost","data15","aJrHfybLxsLU76rV","data15");
+if($my->mysql_errno){
+  die("MySQL, virhe yhteyden luonnissa:".$my->connect_error);
+}
+$my->set_charset('utf8');
+$sql="
+SELECT * FROM asuntotieto
+WHERE $hakua LIKE \"$hakub\";";
 
+if($tulos = $my->query($sql)){
+    while($d = $tulos->fetch_object()){
+    echo "<tr><td>$d->osoite</td><td>$d->pnro</td><td>$d->kaupunki</td><td>$d->pintaala</td><td>$d->hinta</td><td>$d->tyyppi</td><td>$d->kerros</td><td>$d->asuntonro</td><td>$d->esittely</td><td>$d->rakennusvuosi</td></tr>";
+    }
+}else{}
+
+$my->close();
+?>
+</tbody>
+</table></div>
       <div class="jumbotron">
       <table border="solid">
     </div>
     </div>
-    <div class="col-md-6 col-lg-6 col-xs-12">
-    </div>
-    <div class="col-md-6 col-lg-6 col-xs-12">
+    <div class="col-md-12 col-lg-12 col-xs-12 table-responsive" style="padding-top: 1cm;">
+        <table>
+        <tbody>
         <?php
 		     $my=mysqli_connect("localhost","data15","aJrHfybLxsLU76rV","data15");
     if ($my->mysql_errno) {
@@ -75,7 +114,10 @@
   } else {
     echo "Virhe SQL-kyselyssä!";
     }
+
 ?>
+</tbody>
+</table>
 </div>
 
 
