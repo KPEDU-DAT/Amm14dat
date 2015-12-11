09eb4d9a7f77592bf9bb6fbc4603c91f94eac723
Johannes Kallinen <johannes.kallinen@kpedu.fi>
2015-12-11 07:52:30 +0200

    toimii

--- team1/asukastietojenmuutto.php
+++ team1/asukastietojenmuutto.php
@@ -7,32 +7,17 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
-    <title>JS Asunto Oy</title>
-    <link href="/~toniahola/bs2015/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="jumbotron.css" rel="stylesheet">
+    <title>Bootstrap pohjatiedosto</title>
+    <link href="/~johanneskallinen/bs2015/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/~johanneskallinen/bs2015/pohja.css" rel="stylesheet">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
-	<body>
-		<nav class="navbar navbar-inverse navbar-fixed-top">
-			<div class="container">
-				<div class="navbar-header">
-					<ul class="nav nav-pills pull-right">
-						<li role="presentation" class="active"><a href="http://cosmo.kpedu.fi/~toniahola/Amm14dat/team1/JS.php#">JS Asunto Oy</a></li>
-            <li role="presentation"><a href="http://cosmo.kpedu.fi/~toniahola/Amm14dat/team1/JSi.php#">Isännöintitodistus</a></li>
-            <li role="presentation"><a href="#">Meistä</a></li>
-            <li role="presentation"><a href="#">Yhteistiedot</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    <div class="container">      
-			<div class="jumbotron">
-				<h1>JS Asunto Oy</h1>
-				<div class="container">
+  <body>
+    <div class="container">
 					<?php
 						$my=mysqli_connect("localhost","data15","aJrHfybLxsLU76rV","data15");
 						if($my->mysql_errno) {
@@ -103,20 +88,9 @@
 								</div>
 							</form>
 						</div>
-					</div>      
-				</div>
-			</div>
-      <div class="row marketing">
-        <div class="col-lg-6">
-          <h4>Subheading</h4>
-          <p>text</p>
-        </div>
-      </div>
-      <footer class="footer">
-        <p>&copy; 2015 JS Asunto, Oy.</p>
-      </footer>
-    </div>
-    <script src="/~toniahola/bs2015/bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="/~toniahola/bs2015/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+						</div>
+					    </div><!-- /.container -->
+    <script src="/~johanneskallinen/bs2015/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="/~johanneskallinen/bs2015/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
   </body>
 </html>
