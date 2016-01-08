e6255e34974f1006f1a0ee828774e35c68719aaa
Johan Handelin <johanhandelin@cosmo.kpedu.fi>
2016-01-08 08:34:53 +0200

    v.1layout

---
+++ SILMU/siLMUjohan.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+    <link href="/~jonashandelin/bs_2015/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>SILMU</title>
+    <link href="jumbotron.css" rel="stylesheet">
+    <link rel="stylesheet" href="kek.css">
+    </head>
+    <body>
+        <div class="row">
+	      <div class="col-md-3">
+	        <div class="lista2">
+	        <div class="lista1">
+	         <!--  <h1>SIVU</h1> -->
+            </div>
+            </div>
+          </div>
+	      <div class="col-md-9">
+          
+	        <!-- <h1>Pääjuttu</h1> -->
+	        <div class="row paa">
+	         <div class="col-md-2"><button class="btn btn-default">Lisää kuva</button></div>
+	         <div class="col-md-2"><button class="btn btn-default">Lisää ääni</button></div>
+	         <div class="col-md-2 col-md-offset-5"> <button class="btn-lg btn btn-primary al">Aloita esitys</button></div>
+	        </div>
+	        <div class="row">
+	          <div class="ruutu">
+	          </div>
+	        </div>
+	           
+	      </div>
+	        
+	
+  
+    
+    
+    
+    
+    
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="/~jonashandelin/bs_2015/bower_components/bootstrap/dist/js//bootstrap.min.js"></script>
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+    </body>
+    </html>
